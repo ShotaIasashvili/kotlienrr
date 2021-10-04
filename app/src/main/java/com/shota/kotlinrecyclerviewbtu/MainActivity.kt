f0716a5fa7a77182fdb80ef55ffcd7b9133dfaa5
ShotaIasashvili <shotaiasashvili@gmail.com>
2021-10-05 03:10:09 +0400

    Initial commit

---
+++ app/src/main/java/com/shota/kotlinrecyclerviewbtu/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.shota.kotlinrecyclerviewbtu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinrecyclerviewbtu.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvContacts.adapter = ContactAdapter(this, createContacts())
+        rvContacts.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun createContacts(): List<Contact> {
+        val contacts = mutableListOf<Contact>()
+        for (i in 1..150) contacts.add(Contact("Person #$i", i))
+        return contacts
+    }
+}
